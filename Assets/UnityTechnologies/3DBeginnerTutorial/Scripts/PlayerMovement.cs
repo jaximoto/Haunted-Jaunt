0b4fa30cc4af5da03fac69b13b5ac880b62b779a
jaximoto <62913915+jaximoto@users.noreply.github.com>
2023-04-19 23:59:30 -0700

    Particles

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
@@ -4,25 +4,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform enemy;
+    public ParticleSystem Dust;
     public float turnSpeed = 20f;
-    public ParticleSystem dust;
+    public float moveSpeed;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     // A variable to store rotation
     Quaternion m_Rotation = Quaternion.identity;
-    // Start is called before the first frame update
 
     void CreateDust()
     {
-        dust.Play();
+        Dust.Play();
     }
+    // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
+
+
+     void Update()
+    {
+        if (enemy)
+        {
+            // use dot product to get positional information about player in respect to enemy
+            Vector3 dToEnemy = enemy.transform.position - transform.position;
+            Vector3 d = enemy.transform.position - transform.position;
+            dToEnemy.Normalize();
+            float dot = Vector3.Dot(Vector3.forward, dToEnemy);
+
+            // set up interpolation for setting speed
+            float maxSpeed = 2.5f;
+            float minSpeed = 1.0f;
+            float alpha = 0f;
+
+            // if player is in line of sight of enemy and within the given distance, speed increases
+            if ((dot > 0) && (d.magnitude < 5))
+            {
+                alpha = 1f - dot;
+                //Debug.Log("You're close to an enemy!");
+               
+            }
+            else
+            {
+                alpha = 0f;
+                //Debug.Log("You're safe.");
+            }
+
+            // use alpha set above to increase or decrease speed based on position
+            moveSpeed = (1.0f - alpha) * minSpeed + alpha * maxSpeed;
+            Debug.Log(moveSpeed);
+
+        }
+        
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -41,10 +81,12 @@ public class PlayerMovement : MonoBehaviour
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
+
         if (isWalking)
         {
             CreateDust();
         }
+
         // Set the bool of the animator component accordingly
         m_Animator.SetBool("IsWalking", isWalking);
 
@@ -59,10 +101,10 @@ public class PlayerMovement : MonoBehaviour
     void OnAnimatorMove()
     {
         // Moving RigidBody to new position + the m_movement vector multiplied by the magnitude of the Animator's deltaPosition
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude * moveSpeed);
 
         m_Rigidbody.MoveRotation(m_Rotation);
-        
     }
 
 }
+
